ee0841e63932c8e5d731c133e21ddc75d2445d72
Bibest <Bibhestee@gmail.com>
2022-06-29 12:56:28 +0000

    Modify 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,8 @@
 for a in range(0, 10):
     for b in range(0, 10):
         if a != b and b > a:
-            print("{}{}".format(a,b), end="")
+            print("{}{}".format(a, b), end="")
             if (a + b) != 17:
-                 print(", ", end="")
+                print(", ", end="")
             else:
                 print("")
