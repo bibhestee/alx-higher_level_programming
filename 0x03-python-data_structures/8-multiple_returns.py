da9ed9741d331261c3bf5c26b88de202b558dd96
Bibest <Bibhestee@gmail.com>
2022-07-02 12:25:30 +0000

    Modify 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,9 +3,8 @@
 
 def multiple_returns(sentence):
     length = len(sentence)
-    ch = sentence[0]
     if len(sentence) < 1:
         tuple = (length, None)
     else:
-        tuple = (length, ch)
+        tuple = (length, sentence[0])
     return tuple
