3af8be70692efe9072eebc58eb9f384069c7f098
Bibest <bibhestee@gmail.com>
2022-07-18 19:48:43 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
             else:
                 print(my_list[n])  # Add new line to the last element
             ele += 1
-        return x
+        return ele
     except BaseException:
         return ele
