61fafaef11ce316b779de2a40ebf3531450525aa
Bibest <Bibhestee@gmail.com>
2022-07-18 17:31:21 +0000

    Create and Update 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    ele = 0  # Number of elements
+    try:
+        for n in range(0, x):
+            if my_list[n] != my_list[-1]:  # If not the last element
+                print(my_list[n], end="")
+            else:
+                print(my_list[n])  # Add new line to the last element
+            ele += 1
+        return x
+    except Exception:
+        return ele
