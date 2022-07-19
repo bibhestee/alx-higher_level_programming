5cc42f8c5c7db05b7058206f9a4bb0ef39030755
Bibest <Bibhestee@gmail.com>
2022-07-19 17:47:43 +0000

    Create and Update 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    This module defines a class Square with private instances
+    attribute size and raise exceptions """
+
+
+class Square:
+
+    """
+    Square (class): create a square with the size specified by
+    the parameter size and raise error if type is not int or
+    size is less than zero.
+    Attributes:
+        size (int): specify the size of the square.
+    Args:
+        size (int): size of the square.
+    Raises:
+        ValueError: if size is less than zero(0).
+        TypeError: if size is not an integer.  """
+
+    def __init__(self, size=0):
+        try:
+            self.__size = size
+            if size < 0:
+                raise ValueError("size must be >= 0")
+        except TypeError:
+            raise TypeError("size must be an integer")
