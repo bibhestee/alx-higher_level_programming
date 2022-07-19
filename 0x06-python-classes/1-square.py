85316fbcd66df157fe326cf3fc101e1a5319ebb0
Bibest <Bibhestee@gmail.com>
2022-07-19 12:51:12 +0000

    Modify 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+    This module creates an empty class Square that defines a square
+"""
+
+
 class Square:
+    """
+    The class Square uses the size argument to create a square
+    The __init__ method create a private instance variable size
+        Args:
+            size (int): Takes an integer."""
     def __init__(self, size):
         self.__size = size
