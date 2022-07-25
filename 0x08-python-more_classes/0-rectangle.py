05927ccc00cdc2bc44c73fc79aff62eb8ff7df21
Bibest <Bibhestee@gmail.com>
2022-07-25 16:56:21 +0000

    Create and Update 0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    module to create an emply class
+"""
+
+
+class Rectangle():
+
+    """
+    Rectangle - empty class
+    Attributes:
+    Args:
+    Raises:
+    """
+
+    pass
