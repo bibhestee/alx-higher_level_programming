b0edf62e4a7cef7bf29d55dc6bf5c51c7d47a62b
Bibest <bibhestee@gmail.com>
2022-08-01 21:19:03 +0100

    Add new line before function declaration

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,6 +3,7 @@
     Module to check if object is an instance of a specified class
 """
 
+
 def is_same_class(obj, a_class):
     """
        is_same_class - returns true if obj is an instance of a_class
