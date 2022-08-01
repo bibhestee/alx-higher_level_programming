f453faeee697b96cd6733750d42194dcc5321e73
Bibest <bibhestee@gmail.com>
2022-08-01 20:12:28 +0100

    Modify 2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,9 +4,4 @@
 """
 
 def is_same_class(obj, a_class):
-    if obj is None:
-        return False
-    elif a_class is None:
-        return False
-    else:
-        return issubclass(a_class, type(obj))
+    return issubclass(a_class, type(obj))
