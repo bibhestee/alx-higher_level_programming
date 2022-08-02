a4d35fa828d706d3482f5273a7a386bac66da0c1
Bibest <Bibhestee@gmail.com>
2022-08-02 10:25:48 +0000

    BaseGeometry added instead of importing

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,14 +1,37 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """
    module to create a rectangle from base geometry
 """
+
+class BaseGeometry:
+    """
+       BaseGeometry (class): create a geometry
+       Attributes:
+           area(): raise exception
+       Raises:
+           TypeError: <name> must be an integer
+           ValueError: <name> must be greater than 0
+           Exception: area() is not implemented
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
     """
        Rectangle (class): create a rectangle from base geometry
        Attributes:
+           area(): raise exception
+           integer_validator(): validate the input
+       Arguments:
+           width (private): width of the rectangle
+           height (private): height of the rectangle
     """
     def __init__(self, width, height):
         if self.integer_validator("width", width):
