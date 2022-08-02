d44e366ec209ffed9d8644bc03c20346d879e15f
Bibest <Bibhestee@gmail.com>
2022-08-02 15:30:37 +0000

    Create and Update

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+   module of a function that reads a text file and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+       read_file - read file
+       Args:
+            filename (file): file to be read
+    """
+    with open(filename, encoding="utf-8") as File:
+        print(File.read())
