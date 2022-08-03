dd0967736cf5ba8f25a67c32f084d44947430ef2
Bibest <Bibhestee@gmail.com>
2022-08-03 16:21:42 +0000

    Create 1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+   module of a function that writes to a text file and returns the number of characters written
+"""
+
+
+def write_file(filename="", text=""):
+    """
+       write_file - write to a file
+       Args:
+            filename (file): file to be write to
+            text (str): string
+    """
+    with open(filename, 'w', encoding="utf-8") as File:
+        return File.write(text)
