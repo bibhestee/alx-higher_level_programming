ac3eed4f099b52ee6ca8593333e854742ec6c5a5
Bibest <Bibhestee@gmail.com>
2022-08-06 14:18:12 +0000

    Create models package

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+   Module to create base class
+"""
+
+
+class Base:
+    """
+       Base - Base class
+       Args:
+            id (int): id number of instance
+            __nb_objects (int): nb of instance(objects)
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
