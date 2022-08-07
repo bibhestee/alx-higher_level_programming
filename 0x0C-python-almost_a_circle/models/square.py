857bc969d8fa307dae2096f1ab46d7ada79cf2a1
Bibest <Bibhestee@gmail.com>
2022-08-07 11:59:25 +0000

    Update: improve update method to include kwargs

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -35,3 +35,44 @@ class Square(Rectangle):
         """ Prints string when instance is called by name """
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
                                                  self.width)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.width = value
+            self.height = value
+
+    def update(self, *args, **kwargs):
+        """ Update by assigning the args or kwargs to attrs """
+        if args:
+            if len(args) == 4:
+                self.id = args[0]
+                self.width = self.height = args[1]
+                self.x = args[2]
+                self.y = args[3]
+            if len(args) == 3:
+                self.id = args[0]
+                self.width = self.height = args[1]
+                self.x = args[2]
+            if len(args) == 2:
+                self.id = args[0]
+                self.width = self.height = args[1]
+            if len(args) == 1:
+                self.id = args[0]
+        else:
+            if kwargs.__contains__("id"):
+                self.id = kwargs.get("id")
+            if kwargs.__contains__("size"):
+                self.width = self.height = kwargs.get("size")
+            if kwargs.__contains__("x"):
+                self.x = kwargs.get("x")
+            if kwargs.__contains__("y"):
+                self.y = kwargs.get("y")
