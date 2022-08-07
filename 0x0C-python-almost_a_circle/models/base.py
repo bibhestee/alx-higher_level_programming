d32a79c3435337b25641a6a38014cd7e38805596
Bibest <Bibhestee@gmail.com>
2022-08-07 16:30:42 +0000

    Update: add to_json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,16 @@
 """
    Module to create base class
 """
+from json import dumps
 
 
 class Base:
     """
        Base - Base class
+       Attributes:
+                  __nb_objects (int): number of instance
+                  id (int): id number of instance
+                  to_json_string(): convert dict to json string
        Args:
             id (int): id number of instance
             __nb_objects (int): nb of instance(objects)
@@ -18,3 +23,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ Convert list of dictionaries to JSON string """
+        if list_dictionaries:
+            return dumps(list_dictionaries)
+        else:
+            return "[]"
