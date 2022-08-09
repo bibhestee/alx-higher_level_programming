b4f1d481bd938fefa6e628bf165393440b924fce
Bibest <Bibhestee@gmail.com>
2022-08-09 10:47:16 +0000

    Modify: raise exception for width/height is under or equal to 0.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -33,13 +33,13 @@ class Rectangle(Base):
         super().__init__(id)
         if type(width) is not int:
             raise TypeError("width must be an integer")
-        elif width < 0:
+        elif width <= 0:
             raise ValueError("width must be > 0")
         else:
             self.__width = width
         if type(height) is not int:
             raise TypeError("height must be an integer")
-        elif height < 0:
+        elif height <= 0:
             raise ValueError("height must be > 0")
         else:
             self.__height = height
@@ -76,7 +76,7 @@ class Rectangle(Base):
     def width(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        elif value <= 0:
             raise ValueError("width must be > 0")
         else:
             self.__width = value
@@ -85,7 +85,7 @@ class Rectangle(Base):
     def height(self, value):
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        elif value <= 0:
             raise ValueError("height must be > 0")
         else:
             self.__height = value
