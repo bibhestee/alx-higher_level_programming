cbf6eb7a56e89b7349cb343e51759ff3240a7048
Bibest <Bibhestee@gmail.com>
2022-09-07 19:50:20 +0000

    describe a table

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- prints the full description of the table first_table from the database specified
+-- database name will be passed as an argument of the mysql command
+SHOW COLUMNS FROM first_table;
