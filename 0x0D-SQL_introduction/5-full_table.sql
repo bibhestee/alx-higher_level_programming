eb00818dfc5ec14210d4a711e9f445a8c28ab5c4
Bibest <Bibhestee@gmail.com>
2022-09-07 20:12:39 +0000

    describe the first_table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- prints the full description of the table first_table from the database specified
 -- database name will be passed as an argument of the mysql command
-DESC first_table;
+SHOW CREATE TABLE first_table;
