e96081f2bce9a71b1a95f5a7e39267cbbb4150fc
Bibest <Bibhestee@gmail.com>
2022-09-08 12:35:05 +0000

    list score >= 10

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,6 @@
+-- Lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0
+-- Results should display both the score and the name in desc order
+-- The database name will be passed as an argument of the mysql command
+SELECT score, name FROM second_table
+    WHERE score >= 10
+        ORDER BY score DESC;
