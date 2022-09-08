bf42b86d702fe5419fa046e9b074f971bcb203ee
Bibest <Bibhestee@gmail.com>
2022-09-08 12:30:34 +0000

    List records in desc order

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+
+-- lists all records of the table second_table of the database hbtn_0c_0
+-- Results should display both the score and the name in descending order
+-- The database name will be passed as an argument of the mysql command
+SELECT score, name FROM second_table
+    ORDER BY score DESC;
