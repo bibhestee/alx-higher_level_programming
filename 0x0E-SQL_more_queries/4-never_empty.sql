af0470ec384cea18b7937bbb01a08ff0dbfef7a0
Bibest <Bibhestee@gmail.com>
2022-09-08 18:23:34 +0000

    set default value to col

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,7 @@
+-- creates the table id_not_null
+-- description:
+--      id int with the default value 1
+--      name VARCHAR(256)
+-- The database name will be passed as an argument of the mysql command
+-- ignore if the table id_not_null already exists
+CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256));
