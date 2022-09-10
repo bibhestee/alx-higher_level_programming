16f77c4030b075b1073d2a71fa4b9b38ad0d888d
Bibest <Bibhestee@gmail.com>
2022-09-10 11:13:03 +0000

    Right join with condition

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -7,8 +7,10 @@
 -- Results must be sorted in descending order by the number of shows linked
 -- Only on SELECT statement is allowed
 -- The database name will be passed as an argument of the mysql command
-SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows FROM tv_show_genres
-RIGHT JOIN tv_genres
-ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.id, genre_id
-ORDER BY number_of_shows DESC;
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
+    FROM tv_show_genres
+    RIGHT JOIN tv_genres
+    ON tv_genres.id = tv_show_genres.genre_id
+        WHERE tv_show_genres.genre_id IS NOT NULL
+    GROUP BY tv_genres.id, genre_id
+    ORDER BY number_of_shows DESC;
