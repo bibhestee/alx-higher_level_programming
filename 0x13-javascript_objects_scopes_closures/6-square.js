843598b4cdfef9b63d41db5a49506d841c21a21e
Bibest <Bibhestee@gmail.com>
2022-10-05 14:33:55 +0000

    Add a new instance method charPrint that prints with 'C'.

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (!c) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      let sq = c;
+      for (let j = 1; j < this.height; j++) {
+        sq += c;
+      }
+      console.log(sq);
+    }
+  }
+};
