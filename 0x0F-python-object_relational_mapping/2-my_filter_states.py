f0dae46f0b9b744f831b15e8bde5b9cae03d11ea
Bibest <Bibhestee@gmail.com>
2022-10-06 13:53:37 +0000

    Lists all states that matches the arg passed from the db using MySQLdb

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    This is a script that lists all states with a name
+    that matches the argument passed from the database hbtn_0e_0_usa
+    using MySQLdb
+"""
+
+
+def my_filter_states(MY_USER, MY_PASS, MY_DB, NAME):
+    """ Function that filter and print the states """
+    import MySQLdb
+
+    database = MySQLdb.connect(user=MY_USER, passwd=MY_PASS, db=MY_DB)
+    cur = database.cursor()
+    cur.execute('SELECT * FROM states WHERE name = \'{}\';'.format(NAME))
+    items = cur.fetchall()
+    for item in items:
+        print(item)
+    cur.close()
+    database.close()
+
+
+if __name__ == "__main__":
+    import sys
+    usr = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+    st = sys.argv[4]
+    my_filter_states(usr, pwd, db, st)
