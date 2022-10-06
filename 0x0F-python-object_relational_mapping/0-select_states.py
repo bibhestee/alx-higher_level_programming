b9cf6f1eec2b20ed5d2d032bd3c2e0b2d93bad7e
Bibest <Bibhestee@gmail.com>
2022-10-06 13:04:16 +0000

    Lists all states from the db using MySQLdb

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+  This is a script that lists all states from the database hbtn_0e_0_usa
+    using MySQLdb ORM
+"""
+
+
+def select_states(MY_USER, MY_PASS, MY_DB):
+    """ Function that print the states """
+    import MySQLdb
+
+    database = MySQLdb.connect(user=MY_USER, passwd=MY_PASS, db=MY_DB)
+    cur = database.cursor()
+    cur.execute("SELECT * FROM states")
+    items = cur.fetchall()
+    for item in items:
+        print(item)
+
+
+if __name__ == "__main__":
+    import sys
+    usr = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+    select_states(usr, pwd, db)
