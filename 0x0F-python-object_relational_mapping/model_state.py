c41249e42d6158f23998829dc15b2f21c1e42e2d
Bibest <bibhestee@gmail.com>
2022-10-08 11:05:44 +0000

    Base model for State table

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,24 @@
+"""
+    This script define a class of a State and an instance
+    Base = declarative_base()
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+        State - State class
+        Attributes:
+                    id = state id
+                    name = state name
+                    __tablename__ = table name
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, autoincrement=True,
+                unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
