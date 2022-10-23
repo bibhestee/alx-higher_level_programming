11d9c2697e1a88f847479fea896bbf53a907a676
Bibest <bibhestee@gmail.com>
2022-10-23 12:14:53 +0000

    Finds a peak in a list of unsorted integers

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# This script finds a peak in a list of unsorted integers
+
+def find_peak(list_of_integers):
+    # get lists of integers and return the peak
+    if list_of_integers:
+        nums = len(list_of_integers)
+        peak = 0
+        for i in range(nums):
+            if list_of_integers[i] > peak:
+                peak = list_of_integers[i]
+        return peak
+    return None
