d54b95af181a4f769c535e2d853e827b27e4412f
Bibest <bibhestee@gmail.com>
2022-11-02 14:46:39 +0000

    Fetch the information of a url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@
 
 
 def hbtn_status():
-    """ This functions fetches the url and return the data as bytes file """
+    """ This functions fetches the url and return the data as a bytes file """
     from urllib import request
 
     req = request.Request('https://alx-intranet.hbtn.io/status')
