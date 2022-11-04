95f4c98cc857bfdb6f1118f572ca4f74a110b1b5
Bibest <bibhestee@gmail.com>
2022-11-04 09:07:38 +0000

    POST request: get information from an api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+    This python script takes in a letter and sends a POST request to
+    http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+
+
+def json_api():
+    """
+        json_api : get information from an api and return json data
+        Requirement:
+            - The input must be sent in the variable q
+            - The output format should be [<id>] <name>
+    """
+
+    import sys
+    import requests
+
+    url = "http://0.0.0.0:5000/search_user"
+
+    if len(sys.argv) > 1:
+        value = sys.argv[1]
+    else:
+        value = ""
+    data = {'q': value}
+
+    # Post a request to the api
+    response = requests.post(url, data=data)
+    content = response.json()
+    if not content:
+        print("No result")
+    else:
+        name = content.get('name')
+        id = content.get('id')
+        if not name or not id:
+            print("Not a valid JSON")
+        else:
+            print(f"[{id}] {name}")
+
+
+if __name__ == "__main__":
+    json_api()
