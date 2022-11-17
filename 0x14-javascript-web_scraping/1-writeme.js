b5a5d795fe2429dfcf36eb0ec84e97fbb9a871b4
Bibest <bibhestee@gmail.com>
2022-11-17 12:36:06 +0100

    Writeme

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Import the fs module then call the writeFile method
+const fs = require('fs');
+
+// Filename and string to write
+let filename = process.argv[2];
+const string = process.argv[3];
+
+if (!filename) { filename = ''; }
+
+fs.writeFile(filename, 'utf8', string, (err, data) => {
+  if (err) {
+    console.error(err);
+  }
+});
