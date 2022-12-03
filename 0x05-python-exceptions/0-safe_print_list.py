569755938e125100ce3d37fc6d95cc5bebcd05a4
bibest <bibhestee@ggmail.com>
2022-12-03 06:04:13 -0800

    Safe print list: Use try and except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,6 +2,9 @@
 def safe_print_list(my_list=[], x=0):
     ele = 0  # Number of elements
     try:
+        if x == 0:
+            print()
+            return x
         for n in range(0, x):
             if my_list[n] != my_list[-1] and n != (x - 1):
                 print(my_list[n], end="")
