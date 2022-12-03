4071473d87c1ed1ed31a5e332773213725478d58
bibest <bibhestee@ggmail.com>
2022-12-03 07:44:27 -0800

    Safe print list integers: validate list item type

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,12 +6,14 @@ def safe_print_list_integers(my_list=[], x=0):
             print()
             return x
         for n in range(0, x):
-            if type(my_list[n]) is not int: continue
+            if type(my_list[n]) is not int:
+                continue
             if my_list[n] != my_list[-1] and n != (x - 1):
                 print("{:d}".format(my_list[n]), end="")
                 num_of_element += 1
             else:
-                print("{:d}".format(my_list[n]))  # Add new line to the last num_of_elementment
+                # Add new line to the last element
+                print("{:d}".format(my_list[n]))
                 num_of_element += 1
         return num_of_element
     except IndexError:
