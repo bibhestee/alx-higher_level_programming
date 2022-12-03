58cbffb7acf15d614cf3b695cb2f425474c9e494
bibest <bibhestee@ggmail.com>
2022-12-03 07:11:11 -0800

    Safe_print_list_integers: filter list item type

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    ele = 0  # Number of elements
+    num_of_element = 0  # Number of num_of_elementments
     try:
+        if x == 0:
+            print()
+            return x
         for n in range(0, x):
-            if my_list[n]:
-                if my_list[n] != my_list[-1] and n != (x - 1):
-                    print("{:d}".format(my_list[n]), end="")
-                    ele += 1
-                else:
-                    print("{:d}".format(my_list[n]))
-                    ele += 1
+            if type(my_list[n]) is not int: continue
+            if my_list[n] != my_list[-1] and n != (x - 1):
+                print(my_list[n], end="")
+                num_of_element += 1
             else:
-                continue
-        return ele
-    except IndexError:
-        return ele
+                print(my_list[n])  # Add new line to the last num_of_elementment
+                num_of_element += 1
+        return num_of_element
     except BaseException:
-        return ele
+        return num_of_element
